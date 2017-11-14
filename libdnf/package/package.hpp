1fccbfd813681521d2bd80b9807787d40a0a4744
Michal Domonkos <mdomonko@redhat.com>
2017-11-14 11:27:29 +0100

    doc: add example API ref

--- libdnf/package/package.hpp
+++ libdnf/package/package.hpp
@@ -3,14 +3,39 @@
 
 #include <string>
 
+/// Class representing a package.
+/**
+ * Here goes the detailed description.
+ * It can span multiple lines in the comment block.
+ *
+ * Paragraphs are delimited with blank lines.
+ * Markdown is supported.
+ */
 class Package {
 public:
+
+    /// Default contructor.
+    /** A more elaborate description of the constructor. */
     Package() = default;
 
+    /// Default destructor.
+    /** A more elaborate description of the destructor. */
     virtual ~Package() = default;
 
     // class is small enough for now, is it ok to leave getters in header ??
+
+    /// Returns the name of this package.
+    /**
+     * A more elaborate description of the getter.
+     * \return the name of this package
+     */
     const std::string &getName() const { return name; }
+
+    /// Sets the name of this package.
+    /**
+     * A more elaborate description of the setter.
+     * \param value the string to use as the new name
+     */
     void setName(const std::string &value) { name = value; }
 
 protected:
