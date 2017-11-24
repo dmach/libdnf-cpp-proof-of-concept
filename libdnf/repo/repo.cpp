75272553a468b4152628c0f37a1353330f16fc6f
Jaroslav Rohel <jrohel@redhat.com>
2017-11-24 11:15:31 +0100

    Hide implementation of Repo class
    PImpl idiom: Remove implementation details of a class from its object representation
    by placing them in a separate class, accessed through an opaque pointer.

--- libdnf/repo/repo.cpp
+++ libdnf/repo/repo.cpp
@@ -1,42 +1,46 @@
 #include <string>
 #include <vector>
 
-#include "repo.hpp"
+#include "repo-private.hpp"
 
 using namespace std;
+
+Repo::Repo() : pimpl{new Impl} {}
+
+Repo::~Repo() = default;
 
 void
 Repo::set_repoid(const string &value)
 {
-    repoid = value;
+    pimpl->set_repoid(value);
 }
 
 const string &
 Repo::get_repoid() const
 {
-    return repoid;
+    return pimpl->get_repoid();
 }
 
 void
 Repo::set_baseurl(const vector<string> &value)
 {
-    baseurl = value;
+    pimpl->set_baseurl(value);
 }
 
 const vector<string> &
 Repo::get_baseurl() const
 {
-    return baseurl;
+    return pimpl->get_baseurl();
 }
 
 const std::vector<Package *> &
 Repo::getPackages() const
 {
-    return packages;
+    return pimpl->getPackages();
 }
 
 void
 Repo::setPackages(vector<Package *> &value)
 {
-    packages = value;
+    pimpl->setPackages(value);
 }
